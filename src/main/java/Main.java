94090e886e0522deff44189876833315741d9e38
zl8244 <zl8244@g.rit.edu>
2024-08-20 10:29:50 -0400

    Can now assign number of bombers and launch mission

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,17 +8,32 @@ public class Main {
 
         Mission[] missions = missionFactory.getMissions();
 
-        System.out.println("Pick a mission:");
-        for(int i = 0; i < missions.length; i++) {
-            System.out.println((i+1) + ") " + missions[i]);
-        }
-        int choice = scan.nextInt();
-        Mission chosenMission = missions[choice-1];
-        System.out.println("Execute the mission at night?");
-        System.out.println("1) Yes\n2) No");
-        int nightConfig = scan.nextInt();
-        chosenMission.setIsNight(nightConfig == 1);
-        System.out.println("Here are the mission details:\n" + chosenMission);
+        boolean confirmLaunch = false;
+        do {
+            System.out.println("Pick a mission:");
+            for (int i = 0; i < missions.length; i++) {
+                System.out.println((i + 1) + ") " + missions[i]);
+            }
+            int choice = scan.nextInt();
+            Mission chosenMission = missions[choice - 1];
+
+            System.out.println("Execute the mission at night?");
+            System.out.println("1) Yes\n2) No");
+            int nightConfig = scan.nextInt();
+            chosenMission.setIsNight(nightConfig == 1);
+
+            System.out.println("How many bombers do you want to assign?");
+            int numBombers = scan.nextInt();
+            chosenMission.setNumBombers(numBombers);
+
+            System.out.println("Here are the mission details:\n" + chosenMission);
+            System.out.println("Confirm launch?\n1) Yes\n2) No");
+            int userConfirm = scan.nextInt();
+            confirmLaunch = userConfirm == 1;
+        } while(!confirmLaunch);
+
+        System.out.println("Godspeed men!");
+
         scan.close();
     }
 }
--- src/main/java/Mission.java
+++ src/main/java/Mission.java
@@ -4,6 +4,7 @@ public class Mission {
     private boolean isNight = false;
     private final double reward;
     private final double baseRisk;
+    private int numBombers;
 
     public Mission(String name, int numSteps, double reward, double baseRisk) {
         this.name = name;
@@ -16,12 +17,21 @@ public class Mission {
         this.isNight = isNight;
     }
 
+    public void setNumBombers(int numBombers) {
+        this.numBombers = numBombers;
+    }
+
+    public int getNumBombers() {
+        return numBombers;
+    }
+
     @Override
     public String toString() {
         return "'" + name + '\'' +
                 ", numSteps=" + numSteps +
                 ", isNight=" + isNight +
                 ", reward=" + reward +
-                ", baseRisk=" + baseRisk;
+                ", baseRisk=" + baseRisk +
+                ", numBombers=" + numBombers;
     }
 }
