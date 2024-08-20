d6d768709030835741c6aa6b5a174816608bb90e
zl8244 <zl8244@g.rit.edu>
2024-08-20 10:22:46 -0400

    Missions can now be configured to run at night

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,22 +8,17 @@ public class Main {
 
         Mission[] missions = missionFactory.getMissions();
 
-        int choice;
-        while(true) {
-            try{
-                System.out.println("Pick a mission:");
-                for(int i = 0; i < missions.length; i++) {
-                    System.out.println((i+1) + ") " + missions[i]);
-                }
-                choice = scan.nextInt();
-                break;
-            } catch (Exception e) {
-                System.out.println("Invalid input, please try again.");
-                scan.next();
-            }
+        System.out.println("Pick a mission:");
+        for(int i = 0; i < missions.length; i++) {
+            System.out.println((i+1) + ") " + missions[i]);
         }
-        System.out.println("You chose: " + choice);
-        System.out.println(missions[choice-1]);
+        int choice = scan.nextInt();
+        Mission chosenMission = missions[choice-1];
+        System.out.println("Execute the mission at night?");
+        System.out.println("1) Yes\n2) No");
+        int nightConfig = scan.nextInt();
+        chosenMission.setIsNight(nightConfig == 1);
+        System.out.println("Here are the mission details:\n" + chosenMission);
         scan.close();
     }
 }
